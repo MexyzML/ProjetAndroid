b1aac8cd2b878eb8866eea013635057489be22c3
MexyzML <marin.legrand@epsi.fr>
2021-02-18 11:42:18 +0100

    Ajout Splash

---
+++ app/src/main/java/com/cours/tp_androidstudio/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.cours.tp_androidstudio;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                HomeActivity.displayActivity(SplashActivity.this);
+                finish();
+            }
+        },2000);
+    }
+
+}
